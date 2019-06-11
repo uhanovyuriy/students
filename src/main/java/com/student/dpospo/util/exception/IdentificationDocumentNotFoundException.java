d10ad0671cd9a2b3729acfbc71cdb0a613b385f5
uhanovyuriy <yorikuh@gmail.com>
2019-06-11 15:42:01 +0200

    Create custom Exceptions

---
+++ src/main/java/com/student/dpospo/util/exception/IdentificationDocumentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.student.dpospo.util.exception;
+
+public class IdentificationDocumentNotFoundException extends RuntimeException {
+
+    public IdentificationDocumentNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/com/student/dpospo/util/exception/NameCreatingDocumentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.student.dpospo.util.exception;
+
+public class NameCreatingDocumentNotFoundException extends RuntimeException {
+
+    public NameCreatingDocumentNotFoundException(String message) {
+        super(message);
+    }
+}
